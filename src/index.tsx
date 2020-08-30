362c62bb4d12eedc982770e871a0619dc42d9858
bentong95923 <bentong95923@gmail.com>
2020-08-30 20:54:23 +0800

    router, remove history package

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch } from "react-router-dom";
+//import { createBrowserHistory } from "history";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import "./assets/scss/material-kit-react.scss?v=1.9.0";
 
@@ -11,16 +11,22 @@ import LandingPage from "./views/LandingPage/LandingPage";
 import ProfilePage from "./views/ProfilePage/ProfilePage";
 import LoginPage from "./views/LoginPage/LoginPage";
 
-var hist = createBrowserHistory();
+//var hist = createBrowserHistory();
 
 ReactDOM.render(
-  <Router history={hist}>
+  /* 
+    Please read the following link to find the difference between the Hash and Browser Router.
+    https://reactrouter.com/web/guides/primary-components#:~:text=At%20the%20core%20of%20every,BrowserRouter%3E%20uses%20regular%20URL%20paths.
+  */
+  //<HashRouter>
+  <BrowserRouter>
     <Switch>
       <Route path="/landing-page" component={LandingPage} />
       <Route path="/profile-page" component={ProfilePage} />
       <Route path="/login-page" component={LoginPage} />
       <Route path="/" component={Components} />
     </Switch>
-  </Router>,
+  </BrowserRouter>,
+  // </HashRouter>,
   document.getElementById("root")
 );
